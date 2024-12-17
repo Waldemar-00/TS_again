2d011de39244b7d2f1a329fb01ee9ad6459fdaf4
ULADZIMIR <01myit@gmail.com>
2024-12-17 19:28:57 +0300

    TSC asserts.ts.

--- types/asserts/asserts.js
+++ types/asserts/asserts.js
@@ -1,12 +1,34 @@
 //* We have an object from some server userFrom
-var userFrom = {
-// name: 'Susan',
-// email: 'sus@gmail.com',
-// age: 47
-};
-function assertUser(obj) {
-    if (typeof obj !== 'object' && obj === null && !('name' in obj))
-        throw new Error("The object doesn't have a data about user.");
+function assertsNumber(value) {
+    if (typeof value !== 'number')
+        throw new Error('The value is not a number!');
 }
-assertUser(userFrom);
-console.log(userFrom.name);
+function getValue(value) {
+    try {
+        assertsNumber(value);
+        return "The value is a number: ".concat(value);
+    }
+    catch (error) {
+        if (error instanceof Error)
+            console.log(error.message);
+    }
+}
+console.log(getValue(111));
+console.log(getValue('111'));
+var assertedKeys = ['name', 'age', 'email'];
+var commingUser = {
+    name: 'Sony',
+    age: 45,
+    email: 'sony@getMaxListeners.com',
+    description: 'Lorem ipsum dolor sit amet',
+};
+function assertsObject(obj) {
+    var truth = assertedKeys.every(function (key) { return key in obj; });
+    // const truth = Object.keys( obj as IAssertedUser ).every( (key, index) => key === assertedKeys[index] )
+    if (typeof obj === 'object' && obj !== null && truth)
+        return;
+    else
+        throw new Error('Comming object not match our IAssertedUser interface!');
+}
+assertsObject(commingUser);
+console.log(commingUser.email);
