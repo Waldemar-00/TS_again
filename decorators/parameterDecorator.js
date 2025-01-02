267ce12a49bac981b41f272b846608c58aa5d498
ULADZIMIR <01myit@gmail.com>
2025-01-02 18:04:08 +0300

    Decorator of parameter. Reflect-metadata. Decorator of the same method.

--- decorators/parameterDecorator.js
+++ decorators/parameterDecorator.js
@@ -11,7 +11,10 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
-class Methods {
+Object.defineProperty(exports, "__esModule", { value: true });
+require("reflect-metadata");
+const POSITIVE_METADATA_KEY = Symbol('POSITIVE_METADATA_KEY');
+class Meta {
     constructor(users) {
         this.users = users;
     }
@@ -20,12 +23,46 @@ class Methods {
     }
 }
 __decorate([
+    ValidatePlus(),
     __param(0, Positive()),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [Number]),
     __metadata("design:returntype", void 0)
-], Methods.prototype, "changeUsersAmount", null);
+], Meta.prototype, "changeUsersAmount", null);
 function Positive() {
     return (target, key, index) => {
+        // console.log( Reflect.getOwnMetadata( "design:type", target, key ) )
+        // console.log( Reflect.getOwnMetadata( "design:paramtypes", target, key ) )
+        // console.log( Reflect.getOwnMetadata( "design:returntype", target, key ) )
+        //! you can get: "design:type", "design:paramtypes", "design:returntype"
+        let existParams = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, key) || [];
+        existParams.push(index);
+        Reflect.defineMetadata(POSITIVE_METADATA_KEY, existParams, target, key);
     };
 }
+function ValidatePlus() {
+    return (target, key, descriptor) => {
+        let changeUsersAmount = descriptor.value;
+        descriptor.value = function (...args) {
+            let params = Reflect.getOwnMetadata(POSITIVE_METADATA_KEY, target, key);
+            if (params.length > 0) {
+                params.forEach((index) => {
+                    try {
+                        if (args[index] < 0)
+                            throw new Error(`The argument: ${args[index]} is less than 0... It must be more than 0! `);
+                        else
+                            return changeUsersAmount?.apply(this, args);
+                    }
+                    catch (error) {
+                        if (error instanceof Error)
+                            console.log(error.message);
+                    }
+                });
+            }
+        };
+    };
+}
+const meta = new Meta(10);
+meta.changeUsersAmount(-1100);
+meta.changeUsersAmount(100);
+console.log(meta.users);
--- decorators/parameterDecorator.ts
+++ decorators/parameterDecorator.ts
@@ -1,8 +1,11 @@
-class Methods
+import 'reflect-metadata'
+const POSITIVE_METADATA_KEY = Symbol('POSITIVE_METADATA_KEY')
+class Meta
 {
     constructor ( public users: number ) { }
 
-    changeUsersAmount (@Positive() users: number): void
+    @ValidatePlus()
+    changeUsersAmount ( @Positive() users: number ): void
     {
         this.users = users
     }
@@ -16,6 +19,45 @@ function Positive ()
         index: number
     ) =>
     {
-
+        // console.log( Reflect.getOwnMetadata( "design:type", target, key ) )
+        // console.log( Reflect.getOwnMetadata( "design:paramtypes", target, key ) )
+        // console.log( Reflect.getOwnMetadata( "design:returntype", target, key ) )
+        //! you can get: "design:type", "design:paramtypes", "design:returntype"
+        let existParams: number[] = Reflect.getOwnMetadata( POSITIVE_METADATA_KEY, target, key ) || []
+        existParams.push( index )
+        Reflect.defineMetadata(POSITIVE_METADATA_KEY, existParams, target, key)
     }
 }
+
+function ValidatePlus ()
+{
+    return (
+        target: Object,
+        key: string | symbol,
+        descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
+    ) =>
+    {
+        let changeUsersAmount = descriptor.value
+        descriptor.value = function (...args: any[])
+        {
+            let params: number[] = Reflect.getOwnMetadata( POSITIVE_METADATA_KEY, target, key )
+            if ( params.length > 0 )
+            {
+                params.forEach( ( index ) =>
+                {
+                    try {
+                        if ( args[ index ] < 0 ) throw new Error( `The argument: ${args[index]} is less than 0... It must be more than 0! ` )
+                        else return  changeUsersAmount?.apply( this, args )
+                    } catch (error) {
+                        if(error instanceof Error) console.log(error.message)
+                    }
+                })
+            }
+        }
+    }
+}
+
+const meta = new Meta( 10 )
+meta.changeUsersAmount( -1100 )
+meta.changeUsersAmount( 100 )
+console.log(meta.users)
